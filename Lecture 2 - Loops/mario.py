eaf04adfde0cb8df5dd6c7a3e4f0a95a8385c734
alanizan <anthonyalabz1@gmail.com>
2024-04-25 07:35:50 -0600

    modified:   mario.py

--- Lecture 2 - Loops/mario.py
+++ Lecture 2 - Loops/mario.py
@@ -33,17 +33,29 @@ def print_square(size):
 
 main()
 
-"""
+
 
 
 
 def main():
     print_square(3)
 
-
 def print_square(size):
-    for i in range(size):
+    for i in range(size):       
         print("#" * size)
 
 main()
 
+"""
+def main():
+    print_square(3)
+
+
+def print_square(size):
+    for i in range(size):
+        print_row(size)
+
+def print_row(width):
+    print("#" * width)
+    
+main()
