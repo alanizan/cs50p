ddfdacf5a93aac6a8dcb59fd340fe35477c20825
alanizan <anthonyalabz1@gmail.com>
2024-04-25 07:17:33 -0600

    modified:   mario.py

---
+++ Lecture 2 - Loops/mario.py
@@ -0,0 +1,47 @@
+"""def main():
+    print_columnn(3)
+
+def print_columnn(height):
+    for _ in range (height):
+        print("#")
+
+main()
+
+def main():
+    print_row(4)
+
+def print_row(width):
+    print("?" * width)
+
+main()
+
+
+def main():
+    print_square(1)
+
+def print_square(size):
+
+    # For each row in square
+    for i in range(size):
+
+        #For each brick in row
+        for j in range(size):
+
+            #Printo brick
+            print("#", end="")
+        print()
+
+"""
+main()
+
+
+def main():
+    print_square(3)
+
+
+def print_square(size):
+    for i in range(size):
+        print("#" * size)
+
+main()
+
