9ec7c9ff01f9a17662b9f4ceac75982b7655e3f3
alanizan <anthonyalabz1@gmail.com>
2024-04-25 06:54:35 -0600

    new file:   loop_3.py

---
+++ Lecture 2 - Loops/loop_3.py
@@ -0,0 +1,11 @@
+students = [
+    {"name": "Hermione", "house": "Griffyndor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Griffyndor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Griffyndor", "patronus": "Jack Russel Terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": "None"}
+
+]
+
+for student in students:
+    print (student["name"], student["house"], student["patronus"], sep=", ")
+
