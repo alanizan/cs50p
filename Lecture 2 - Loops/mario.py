c5a333fdf6dd9fba881b798f728e0982f6fc2160
alanizan <anthonyalabz1@gmail.com>
2024-05-02 07:01:59 -0600

    modified:   Lecture 2 - Loops/mario.py 	Lecture 1 - Conditionals/Problem Set 1/interpreter/ 	Lecture 2 - Loops/hogwarts.py 	Python Problem Sets/

--- Lecture 2 - Loops/mario.py
+++ Lecture 2 - Loops/mario.py
@@ -1,52 +1,48 @@
-"""def main():
-    print_columnn(3)
+# def main():
+#     print_columnn(3)
 
-def print_columnn(height):
-    for _ in range (height):
-        print("#")
+# def print_columnn(height):
+#     for _ in range (height):
+#         print("#")
 
-main()
+# main()
 
-def main():
-    print_row(4)
+# def main():
+#     print_row(4)
 
-def print_row(width):
-    print("?" * width)
+# def print_row(width):
+#     print("?" * width)
 
-main()
-
-
-def main():
-    print_square(1)
-
-def print_square(size):
-
-    # For each row in square
-    for i in range(size):
-
-        #For each brick in row
-        for j in range(size):
-
-            #Printo brick
-            print("#", end="")
-        print()
-
-main()
+# main()
 
 
+# def main():
+#     print_square(1)
+
+# def print_square(size):
+
+#     # For each row in square
+#     for i in range(size):
+
+#         #For each brick in row
+#         for j in range(size):
+
+#             #Printo brick
+#             print("#", end="")
+#         print()
+
+# main()
 
 
+# def main():
+#     print_square(3)
 
-def main():
-    print_square(3)
+# def print_square(size):
+#     for i in range(size):
+#         print("#" * size)
 
-def print_square(size):
-    for i in range(size):       
-        print("#" * size)
+# main()
 
-main()
-
-"""
 def main():
     print_square(3)
 
@@ -55,7 +51,9 @@ def print_square(size):
     for i in range(size):
         print_row(size)
 
+
 def print_row(width):
     print("#" * width)
-    
+
+
 main()
